ec8f58ed620cf6445cc6b7d095d8ef12f96c2b58
Lincoln Pahwa <lpahwa@honestbuildings.com>
2019-09-09 16:30:12 -0400

    initial commit

---
+++ apps/home-page/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'home-page',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/home-page'
+};
